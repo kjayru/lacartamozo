eccd54feaed1e845ee598e53643dcdab8cc94c11
kjayru <wiltinoco@gmail.com>
2018-09-17 17:10:05 -0500

    view admin

---
+++ resources/views/admin/login/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layout.login')
+
+@section('content')
+<div class="login-box">
+
+    <div class="login-logo">
+      <a href="../../index2.html"><b>{{ env('APP_NAME')}}</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg">Inicie Sesión</p>
+  
+      <form action="../../index2.html" method="post">
+        <div class="form-group has-feedback">
+          <input type="email" class="form-control" placeholder="Email">
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" placeholder="Password">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+         
+          <!-- /.col -->
+          <div class="col-xs-12">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+  
+      
+  
+  
+    </div>
+    <!-- /.login-box-body -->
+  </div>
+
+  @endsection
