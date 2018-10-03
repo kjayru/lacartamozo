fb4d0a9f124a680977eccfa800107206155bb0e8
kjayru <wiltinoco@gmail.com>
2018-10-03 13:52:01 -0500

    cambios

--- resources/views/admin/partial/header.blade.php
+++ resources/views/admin/partial/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="/admin" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><img src="/images/logo.png"  width="30"></span>
           <!-- logo for regular state and mobile devices -->
