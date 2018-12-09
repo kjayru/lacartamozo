fa2bf13f1a037efb277a5b4fa635100fcc834405
kjayru <wiltinoco@gmail.com>
2018-12-08 19:13:17 -0500

    detalle

--- database/factories/FranchiseFactory.php
+++ database/factories/FranchiseFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Franchise::class, function (Faker $faker) {
         'city' => $faker->city, 
         'province' => $faker->state,
         'cellphone' =>$faker->phoneNumber,
-        'email' => $faker->unique()->safeEmail,
+        'mail' => $faker->unique()->safeEmail,
         'avatar' => '/franchise/'.\Faker\Provider\Image::image(storage_path().'/app/public/franchise',200,200,'people'),
         'status' => '1',
         'package_id' => App\Package::all()->random()->id,
