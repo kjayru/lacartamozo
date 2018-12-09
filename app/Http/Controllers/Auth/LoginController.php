38c5ef52d1c24f34755f0941981982e2bba96a1f
kjayru <wiltinoco@gmail.com>
2018-12-08 22:34:54 -0500

    auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -17,19 +17,8 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-    use AuthenticatesUsers {
-        attemptLogin as attemptLoginAtAuthenticatesUsers;
-    }
 
-    /**
-     * Show the application's login form.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function showLoginForm()
-    {
-        return view('auth.login');
-    }
+    use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
@@ -46,46 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-     /**
-     * Returns field name to use at login.
-     *
-     * @return string
-     */
-    public function username()
-    {
-        return config('auth.providers.users.field', 'email');
-    }
-
-    /**
-     * Attempt to log the user into the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function attemptLogin(Request $request)
-    {
-        if ($this->username() === 'email') {
-            return $this->attemptLoginAtAuthenticatesUsers($request);
-        }
-        if (! $this->attemptLoginAtAuthenticatesUsers($request)) {
-            return $this->attempLoginUsingUsernameAsAnEmail($request);
-        }
-        return false;
-    }
-
-    /**
-     * Attempt to log the user into application using username as an email.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return bool
-     */
-    protected function attempLoginUsingUsernameAsAnEmail(Request $request)
-    {
-        return $this->guard()->attempt(
-            ['email' => $request->input('username'), 'password' => $request->input('password')],
-            $request->has('remember')
-        );
     }
 }
