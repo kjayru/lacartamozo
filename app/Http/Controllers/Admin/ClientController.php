18862765452e96a6849bb2868e79494aa09231d5
kjayru <wiltinoco@gmail.com>
2018-12-08 20:25:14 -0500

    clientes

--- app/Http/Controllers/Admin/ClientController.php
+++ app/Http/Controllers/Admin/ClientController.php
@@ -21,6 +21,7 @@ class ClientController extends Controller
         //$users = CustomerDetail::all();
         //$clientes = RestaurantDetail::where('customer_detail_id',$user->id)->get();
         $clientes = Client::all();
+        dd($clientes);
         return view('admin.paginas.clientes.index',['clients'=>$clientes]);
     }
 
